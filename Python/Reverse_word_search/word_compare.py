ddae0bdae3a03dc29d202737843a70da4ad6e3a0
knauere <kira.knauere@gmail.com>
2021-11-26 04:25:01 +0200

    Final working version

--- Python/Reverse_word_search/word_compare.py
+++ Python/Reverse_word_search/word_compare.py
@@ -9,21 +9,14 @@ perm_string = perm_list.read()
 # remove symbols , and ' from the list, separate in new lines at spaces
 new_perm = perm_string.replace(',', '').replace('\'', '').replace('[', '').replace(']', '').replace(' ', '\n')
 
-# print new perm string
-# print(new_perm)
-
 # save cleaned perm list into new file
 file = open('new_perm_words.txt', "w")
 file.write(new_perm)
 file.close()
 
 # create array from new files
-perm_file = set(line.strip() for line in open('t_perm.txt'))
-dict_file = set(line.strip() for line in open('t_dict.txt'))
-
-#print(perm_file)
-
-result = ''
+perm_file = set(line.strip() for line in open('new_perm_words.txt'))
+dict_file = set(line.strip() for line in open('words_dictionary.txt'))
 
 for i in perm_file:
     for j in dict_file:
