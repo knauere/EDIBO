966bd72ea1a5b2c8f6a35ebb81eba66ec8d1dc24
knauere <kira.knauere@gmail.com>
2021-12-02 15:39:09 +0200

    Stuck at device.ts IGeoPos

--- JavaScript/gps/models/device.ts
+++ JavaScript/gps/models/device.ts
@@ -13,20 +13,23 @@ export interface IDevice {
 class Device implements IDevice {
 
     private name: string = 'No name device';
+    // ???
+    private points: IGeoPos = 0;
 
     setName(name: string) {
         this.name = name;
     }
 
-    // ????
     listCoordinates(): IGeoPos {
-        return this.listCoordinates()
+        return this.points;
     }
 
     setCoordinates(points: IGeoPos): void {
+        this.points = points;
     }
 
 }
 
 const device: IDevice = new Device();
 device.setName('FirstDevice');
+console.log(device)
