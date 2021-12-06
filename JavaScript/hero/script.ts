16bd76c88b8048729fc3ddd3cb4168763ad2fef8
knauere <kira.knauere@gmail.com>
2021-12-06 15:35:11 +0200

    working one way walk, all direction animation

--- JavaScript/hero/script.ts
+++ JavaScript/hero/script.ts
@@ -1,19 +1,45 @@
 const hero = document.querySelector('.hero');
 
 const heroPos = {
-    x: 0,
     y: 0,
+    x: 0,
 }
 
 hero.addEventListener('keydown', event => {
     console.log('.hero', event);
+    if (event.code === 'ArrowRight') {
+        heroPos.x += 20;
+    }
+    moveHero();
+});
+
+hero.addEventListener('keydown', event => {
+    console.log('.hero', event);
+    if (event.code === 'ArrowLeft') {
+        heroPos.x += -20;
+    }
+    moveHero();
+});
+
+hero.addEventListener('keydown', event => {
+    console.log('.hero', event);
     if (event.code === 'ArrowDown') {
-        heroPos.y += 10;
+        heroPos.y += 20;
+    }
+    moveHero();
+});
+
+hero.addEventListener('keydown', event => {
+    console.log('.hero', event);
+    if (event.code === 'ArrowUp') {
+        heroPos.y += -20;
     }
     moveHero();
 });
 
 const moveHero = () => {
+    hero.style.left = heroPos.x + 'px';
     hero.style.top = heroPos.y + 'px';
-
 }
+
+
